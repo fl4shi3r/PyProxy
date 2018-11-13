494e920514dce7a6e43d2ade868f1be04572f7e7
fl4shi3r <fl4shi3r@gmail.com>
2018-11-13 19:28:37 +0530

    modify main.py file

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import re, os, environment, getpass, apt, npm, git, gsetting, bash
+import re, os, environment, getpass, apt, npm, git, gsetting, bash, dnf
 
 
 
 if os.geteuid() != 0:
     exit('You have to root privileges to run this script.\nPlease try again, this time using "sudo".\nExiting.')
 
-host_ip = '172.31.1.4' #input('Enter the Proxy Host address: ')
-host_port = '8080' # input('Enter the Proxy host port: ')
+host_ip = input('Enter the Proxy Host address: ')
+host_port = input('Enter the Proxy host port: ')
 
-username = 'usesrname' #input('Enter the username: ')
-password_unhexed = 'password'  #getpass.getpass(prompt='Enter the password: ')
+username = input('Enter the username: ')
+password_unhexed = getpass.getpass(prompt='Enter the password: ')
 
-pc_username ='fl4shi3r' #input('Enter the pc username: ')
+pc_username = input('Enter the pc username: ')
 
 def password_hex(password):
     hex_password = []
@@ -23,22 +23,24 @@ def password_hex(password):
     return ''.join(hex_password)
 
 password = password_hex(password_unhexed)
+choice = input('1.set proxy\n2.unset proxy\nEnter your choice(1/2): ')
+if choice == '1':
+    environment.set_proxy(username,password , host_ip,host_port) 
+    apt.set_proxy(username, password, host_ip, host_port)
+    npm.set_proxy( username, password, host_ip, host_port, pc_username)
+    git.set_proxy(username, password, host_ip, host_port, pc_username )
+    gsetting.set_proxy(username, password, host_ip, host_port, pc_username)
+    bash.set_proxy(username, password, host_ip, host_port, pc_username)
+    dnf.set_proxy(username, password, host_ip, host_port)
 
-# environment.set_proxy(username,password , host_ip,host_port) // Set Proxy in Environment file
-# environment.unset_proxy() // Unset Proxy in Environment file
-
-# apt.set_proxy(username, password, host_ip, host_port)
-# apt.unset_proxy()
-
-# npm.set_proxy( username, password, host_ip, host_port, pc_username)
-# npm.unset_proxy(pc_username)
-
-# git.set_proxy(username, password, host_ip, host_port, pc_username )
-# git.unset_proxy(pc_username)
-
-# gsetting.set_proxy(username, password, host_ip, host_port, pc_username)
-# gsetting.unset_proxy(pc_username)
-
-# bash.set_proxy(username, password, host_ip, host_port, pc_username)
-# bash.unset_proxy(pc_username)
+elif choice == '2':
+    environment.unset_proxy() 
+    apt.unset_proxy()
+    npm.unset_proxy(pc_username)
+    git.unset_proxy(pc_username)
+    gsetting.unset_proxy(pc_username)
+    bash.unset_proxy(pc_username)
+    dnf.unset_proxy()
+else:
+    print("Enter the correct option")
 
