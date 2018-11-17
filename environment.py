c801c87a0d8f9d1ad1f616fc7b8830087413ce7a
fl4shi3r <fl4shi3r@gmail.com>
2018-11-17 20:26:53 +0530

    Modify environment.py file

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 import os
 
 
-def set_proxy(username, password, host_ip, host_port):
+def set_proxy(host_ip, host_port, auth_choice, pc_username, username, password):
     file_temp = open('/etc/enviromnent_temp', 'w+')
     file_ob = open('/etc/environment', 'r')
     for line in file_ob.readlines():
@@ -9,10 +9,14 @@ def set_proxy(username, password, host_ip, host_port):
             continue    
         else:
             file_temp.write(line)
-
-    file_temp.write("\n" + 'http_proxy=\'http://'+ username + ':'+ password + '@' + host_ip +':' +  host_port + '\''+ '\n')
-    file_temp.write('https_proxy=\'http://'+ username + ':'+ password + '@' + host_ip + ':' + host_port + '\''+ '\n')
-    file_temp.write('ftp_proxy=\'ftp://'+ username + ':'+ password + '@' + host_ip + ':' + host_port + '\'' + '\n')        
+    if auth_choice == 'yes':
+        file_temp.write("\n" + 'http_proxy=\'http://'+ username + ':'+ password + '@' + host_ip +':' +  host_port + '\''+ '\n')
+        file_temp.write('https_proxy=\'http://'+ username + ':'+ password + '@' + host_ip + ':' + host_port + '\''+ '\n')
+        file_temp.write('ftp_proxy=\'ftp://'+ username + ':'+ password + '@' + host_ip + ':' + host_port + '\'' + '\n')
+    elif auth_choice == 'no':
+        file_temp.write("\n" + 'http_proxy=\'http://' + host_ip +':' +  host_port + '\''+ '\n')
+        file_temp.write('https_proxy=\'http://' + host_ip + ':' + host_port + '\''+ '\n')
+        file_temp.write('ftp_proxy=\'ftp://' + host_ip + ':' + host_port + '\'' + '\n')
     file_ob.close()
     file_temp.close()
     os.remove('/etc/environment')
