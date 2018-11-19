91b90eecc3e793e1223a88cf8e8dc518bcd3ccf2
fl4shi3r <fl4shi3r@gmail.com>
2018-11-19 17:05:31 +0530

    Modify gsetting.py file

--- gsetting.py
+++ gsetting.py
@@ -2,7 +2,7 @@
 import os
 
 def set_proxy(host_ip, host_port, auth_choice, pc_username,  username, password):
-    value = os.system('timeout 0.5s dconf-editor  ')
+    os.system('chmod 0666 /run/user/1000/dconf/user ')
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy mode "manual"')
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy.http host ' + host_ip)
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy.http port ' + host_port)
@@ -18,7 +18,7 @@ def set_proxy(host_ip, host_port, auth_choice, pc_username,  username, password)
 
 
 def unset_proxy(pc_username):
-    os.system('timeout 0.5s dconf-editor ')
+    os.system('chmod 0666 /run/user/1000/dconf/user ')
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy mode "none" ')
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy.http host ""')
     os.system('sudo -u ' + pc_username + ' gsettings set org.gnome.system.proxy.http port 0')
