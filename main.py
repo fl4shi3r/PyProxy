c0595ea156ee10bf79bbcc436659fe1770561652
Shivendra Singh <39083278+fl4shi3r@users.noreply.github.com>
2018-11-23 09:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,38 +22,46 @@ if sys.version_info.major >= 3:
         host_port = input('Enter the Proxy host port: ')
 
         pc_username = input('Enter the pc username: ')
-        username = ''
-        password = ''
-        auth_choice = (input('Is authentication required in your college/organization (yes/no): ')).lower()
-        if auth_choice == 'yes' :
-            username = input('Enter the username: ')
-            password_unhexed = getpass.getpass(prompt='Enter the password: ')
-            password = password_hex(password_unhexed)
-        elif auth_choice == 'no':
-            pass
+        if os.path.isdir('/home/'+pc_username):
+            username = ''
+            password = ''
+            auth_choice = (input('Is authentication required in your college/organization (yes/no): ')).lower()
+            if auth_choice == 'yes' :
+                username = input('Enter the username: ')
+                password_unhexed = getpass.getpass(prompt='Enter the password: ')
+                password = password_hex(password_unhexed)
+            elif auth_choice == 'no':
+                pass
+            else:
+                print('Try again with correct option')
+                exit(1)   
+
+            environment.set_proxy(host_ip, host_port, auth_choice, pc_username, username, password) 
+            apt.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
+            npm.set_proxy( host_ip, host_port, auth_choice,pc_username, username, password)
+            git.set_proxy(host_ip, host_port, auth_choice, pc_username,username, password )
+            gsetting.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
+            bash.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
+            dnf.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
         else:
-            print('Try again with correct option')
-            exit(1)   
-        
-        environment.set_proxy(host_ip, host_port, auth_choice, pc_username, username, password) 
-        apt.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
-        npm.set_proxy( host_ip, host_port, auth_choice,pc_username, username, password)
-        git.set_proxy(host_ip, host_port, auth_choice, pc_username,username, password )
-        gsetting.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
-        bash.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
-        dnf.set_proxy(host_ip, host_port, auth_choice,pc_username, username, password)
+            print("Incorrect pc username. Try again with correct pc username")
+            exit(1)
+            
 
     elif choice == 2:
         import environment, apt, npm, git, gsetting, bash, dnf
         pc_username = input('Enter the pc username: ')
-
-        environment.unset_proxy() 
-        apt.unset_proxy()
-        npm.unset_proxy(pc_username)
-        git.unset_proxy(pc_username)
-        gsetting.unset_proxy(pc_username)
-        bash.unset_proxy(pc_username)
-        dnf.unset_proxy()
+        if os.path.isdir('/home/'+pc_username):
+            environment.unset_proxy() 
+            apt.unset_proxy()
+            npm.unset_proxy(pc_username)
+            git.unset_proxy(pc_username)
+            gsetting.unset_proxy(pc_username)
+            bash.unset_proxy(pc_username)
+            dnf.unset_proxy()
+        else:
+            print("Incorrect pc username. Try again with correct pc username")
+            exit(1)
     else:
         print("Try again with correct option")
     
