00510090ec64a2115ae242f15e3bebd4d7c7bfe2
fl4shi3r <fl4shi3r@gmail.com>
2019-01-28 13:14:18 +0530

    Modify gsetting.py file

--- gsetting.py
+++ gsetting.py
@@ -2,40 +2,34 @@
 import os
 
 def set_proxy(host_ip, host_port, auth_choice, pc_username,  username, password):
-    try:
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy mode "manual"\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http host ' + host_ip + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http port ' + host_port + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https host ' + host_ip + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https port ' + host_port + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp host ' + host_ip + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp port ' + host_port + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks host ' + host_ip + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks port ' + host_port + '\'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http enabled true \'' )
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http use-authentication true \'' )
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-user "' + username + '"\'' )
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-password "' + password + '"\'')
-    except:
-        print("An ERROR occurred, Unable to set proxy in gsettings.")
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy mode "manual"\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http host ' + host_ip + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http port ' + host_port + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https host ' + host_ip + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https port ' + host_port + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp host ' + host_ip + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp port ' + host_port + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks host ' + host_ip + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks port ' + host_port + '\'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http enabled true \'' )
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http use-authentication true \'' )
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-user "' + username + '"\'' )
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-password "' + password + '"\'')
 
 
 
 def unset_proxy(pc_username):
-    try:
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy mode "none" \' ')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http host "" \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http port 0 \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https host "" \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https port 0 \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp host "" \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp port 0 \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks host "" \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks port 0 \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http enabled false \' ' )
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http use-authentication false \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-user "" \'')
-        os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-password "" \'')
-    except:
-        print("An ERROR occurred, Unable to set proxy in gsettings.")
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy mode "none" \' ')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http host "" \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http port 0 \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https host "" \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.https port 0 \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp host "" \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.ftp port 0 \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks host "" \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.socks port 0 \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http enabled false \' ' )
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http use-authentication false \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-user "" \'')
+    os.system('su ' + pc_username +  ' -c \'gsettings set org.gnome.system.proxy.http authentication-password "" \'')
 
